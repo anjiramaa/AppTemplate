08fdd14c7650a8be74fcd0f4642ed1e9ff6fe362
Mike Duffey <mpduffey@gmail.com>
2013-04-22 08:20:54 -0400

    Next Update

---
+++ Codeigniter-Bootstrap-HTML5Boilerplate/application/views/index_head.php
@@ -0,0 +1,75 @@
+		<!-- start: Meta -->
+		<meta charset="utf-8">
+		<meta name="description" content="Strategic Information Consultants Research Engine"/>
+		<meta name="keywords" content="Research" />
+		<meta name="author" content="Mike Duffey"/>
+		<!-- end: Meta -->
+
+		<!-- start: Mobile Specific -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<!-- end: Mobile Specific -->
+
+		<!-- start: Facebook Open Graph -->
+		<meta property="og:title" content=""/>
+		<meta property="og:description" content=""/>
+		<meta property="og:type" content=""/>
+		<meta property="og:url" content=""/>
+		<meta property="og:image" content=""/>
+		<!-- end: Facebook Open Graph -->
+
+		<!-- start: CSS -->
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700"/>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif"/>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo"/>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic"/>
+		<link rel="stylesheet" href="http://blueimp.github.com/Bootstrap-Image-Gallery/css/bootstrap-image-gallery.min.css"/> <!-- Bootstrap Image Gallery -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/bootstrap.css"/> <!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/bootstrap-responsive.css"/> <!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/bootstrap-image-gallery.min.css" media="screen"/> <!-- Bootstrap Image Gallery -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/smoothness/jquery-ui-1.10.2.custom.css"/> <!-- jQuery UI -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/main.css"/> <!-- HTML5 Boilerplate -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/normalize.css"/> <!-- HTML5 Boilerplate -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/font-awesome.css"/> <!-- Font-Awesome -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/font-awesome-ie7.css"/> <!-- Font-Awesome -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/simplePagination.css"/> <!-- simplePagination -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/bootstrap-tables.css"/> <!-- tablecloth.js -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/prettify.css"/> <!-- tablecloth.js -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/tablecloth.css"/> <!-- tablecloth.js -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/bootstrap-notify.css"> <!-- Bootstrap Notify -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/styles/alert-bangtidy.css"> <!-- Bootstrap Notify -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/styles/alert-blackgloss.css"> <!-- Bootstrap Notify -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/styles/alert-notification-animations.css"> <!-- Bootstrap Notify -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/jquery.pnotify.default.css"/> <!-- Pine Notify -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/flexslider.css"/> <!-- FlexSlider 2 -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/jquery.fancybox.css"/> <!-- FancyBox -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/jquery.fancybox-buttons.css?v=1.0.5" media="screen"/> <!-- FancyBox -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/jquery.fancybox-thumbs.css?v=1.0.7" media="screen"/> <!-- FancyBox -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/slider.min.css" media="screen"/> <!-- slider.js -->
+		<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/table_css.css"/> <!-- table_css -->
+
+
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/style.css">
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/parallax-slider.css">
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/jquery.multiselect.css">
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/chosen.css"> <!-- CSS for Chosen plugin -->
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/jquery.fileupload-ui.css" media="screen"/>
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/daterangepicker.css"  /> <!-- css files for daterangepicker -->
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/bootstrap-toggle-buttons.css"> <!-- css file for Bootstrap Toggle Buttons -->
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/prettyCheckable.css"> <!-- css file for PrettyCheckable -->
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/video-default.css"> <!-- css file for tablecloth.js -->
+<!--	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>resources/css/global.css">
+		<!-- Bootstrap CSS fixes for IE6 -->
+        	<!--[if lt IE 7]>
+				<link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-ie6.min.css">
+			<![endif]-->
+			<!--[if IE 7]>
+				<link href="<?= base_url(); ?>resources/css/font-awesome-ie7.css" rel="Stylesheet" type="text/css">
+			<![endif]-->
+        	<!-- CSS adjustments for browsers with JavaScript disabled -->
+        	<noscript><link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/jquery.fileupload-ui-noscript.css"></noscript>
+		<!--[if lt IE 9 ]><link href="<?= base_url(); ?>resources/css/styleIE.css" rel="stylesheet"><![endif]-->
+		<!--[if IE 9 ]><link href="<?= base_url(); ?>resources/css/styleIE9.css" rel="stylesheet"><![endif]-->
+		<!-- end: CSS -->
+
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
---
+++ Codeigniter-Bootstrap-HTML5Boilerplate/application/views/index_script_files.php
@@ -0,0 +1,58 @@
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery-1.9.1.js"></script> <!-- jQuery -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery-ui-1.10.2.custom.js"></script> <!-- jQuery UI -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/bootstrap.js"></script> <!-- Bootstrap -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/bootstrap-image-gallery.min.js"></script> <!-- Bootstrap Image Gallery -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/backbone.js"></script> <!-- Backbone.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/underscore.js"></script> <!-- Underscore.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/json2.js"></script> <!-- json2.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.validate.js"></script> <!-- Bootstrap Validation -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.form.js"></script> <!-- Bootstrap Validation -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.mockjax.js"></script> <!-- Bootstrap Validation -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/spin.min.js"></script> <!-- spin.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/slimScroll.min.js"></script> <!-- slimScroll -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.simplePagination.js"></script> <!-- simplePaginations.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.jeditable.js"></script> <!-- jeditable.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/isotope.js"></script> <!-- isotope.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.tablecloth.js"></script> <!-- tablecloth.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.tablesorter.min.js"></script> <!-- tablecloth.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.metadata.js"></script> <!-- tablecloth.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/list.min.js"></script> <!-- list.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery-defaultButton.js"></script> <!-- jQuery Default Button -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/bootbox.js"></script> <!-- bootbox.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/bootstrap-notify.js"></script> <!-- Bootstrap Notify -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.pnotify.js"></script> <!-- Pine Notify -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.imagesloaded.js"></script> <!-- imagesloaded.js -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.flexslider.js"></script> <!-- FlexSlider 2 -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.fancybox.js"></script> <!-- FancyBox -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.fancybox-buttons.js?v=1.0.5"></script> <!-- FancyBox -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.fancybox-media.js?v=1.0.5"></script> <!-- FancyBox -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/jquery.fancybox-thumbs.js?v=1.0.7"></script> <!-- FancyBox -->
+		<script type="text/javascript" src="<?= base_url(); ?>resources/js/slider.js"></script> <!-- slider.js -->
+
+
+		<script src="<?= base_url(); ?>resources/js/carousel.js"></script> <!-- carousel.js -->
+		<script src="<?= base_url(); ?>resources/js/jquery.placeholder.min.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/excanvas.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.flot.min.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.flot.pie.min.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.flot.stack.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.flot.resize.min.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.multiselect.js"></script> <!--jQuery multiselect combobox js file -->
+		<script src="<?= base_url(); ?>resources/js/chosen.jquery.js"></script> <!-- js file for Chosen plugin -->
+		<script src="<?= base_url(); ?>resources/js/date.js"></script> <!-- js file for daterangepicker plugin -->
+		<script src="<?= base_url(); ?>resources/js/daterangepicker.js"></script> <!-- js file for daterangepicker plugin -->
+		<script src="<?= base_url(); ?>resources/js/tmpl.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/load-image.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/canvas-to-blob.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.iframe-transport.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.fileupload.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.fileupload-ip.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.fileupload-ui.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/locale.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.toggle.buttons.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/prettyCheckable.js"></script> <!-- jQuery UI -->
+		<script src="<?= base_url(); ?>resources/js/jquery.switch.js"></script> <!-- js file for Bootstrap Toggle Buttons -->
+		<script src="<?= base_url(); ?>resources/js/jquery.uriAnchor-1.1.js"></script> <!-- js file for jQuery uirAnchor module -->
+		<script src="<?= base_url(); ?>resources/js/jquery.video-ui.js"></script> <!-- js file for Bootstrap video player -->
+		<script src="<?= base_url(); ?>resources/js/jquery.contextmenu.r2.js"></script> <!-- js file for bootstrap context menu -->
+		<script defer="defer" src="<?= base_url(); ?>resources/js/custom.js"></script>
